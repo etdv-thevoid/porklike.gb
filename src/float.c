bb08dd8cfe15a3189587a25eeb717ae7c2613922
Ben Smith <binjimin@gmail.com>
2025-08-07 16:48:28 -0700

    Fix crash when spawning too many floats

--- src/float.c
+++ src/float.c
@@ -22,23 +22,36 @@ extern const u8 float_diff_y[];
 
 OAM_item_t float_sprites[MAX_FLOATS];
 u8* next_float;
+u8* last_float;  // One past the end.
 
 u8 float_times[MAX_FLOATS];
 u8* next_float_time;
+u8* last_float_time;  // One past the end.
 
 void float_hide(void) {
-  next_float = (u8*)float_sprites;
-  next_float_time = float_times;
+  next_float = last_float = (u8*)float_sprites;
+  next_float_time = last_float_time = float_times;
+}
+
+static void float_add_ex(u8 x, u8 y, u8 tile, u8 prop) {
+  if (next_float == (u8*)(float_sprites + MAX_FLOATS)) {
+    // Wrap back around to overwrite old floats, but keep the last in the same
+    // place.
+    next_float = (u8*)float_sprites;
+    next_float_time = float_times;
+  } else {
+    last_float = next_float + 4;
+    last_float_time = next_float_time + 1;
+  }
+  *next_float++ = y;
+  *next_float++ = x;
+  *next_float++ = tile;
+  *next_float++ = prop;
+  *next_float_time++ = FLOAT_FRAMES;
 }
 
 void float_add(u8 pos, u8 tile, u8 prop) {
-  if (next_float != (u8*)(float_sprites + MAX_FLOATS)) {
-    *next_float++ = POS_TO_Y(pos);
-    *next_float++ = POS_TO_X(pos);
-    *next_float++ = tile;
-    *next_float++ = prop;
-    *next_float_time++ = FLOAT_FRAMES;
-  }
+  float_add_ex(POS_TO_X(pos), POS_TO_Y(pos), tile, prop);
 }
 
 void float_blind(void) {
@@ -46,11 +59,7 @@ void float_blind(void) {
   x = POS_TO_X(mob_pos[PLAYER_MOB]) + FLOAT_BLIND_X_OFFSET;
   y = POS_TO_Y(mob_pos[PLAYER_MOB]);
   for (i = 0; i < 3; ++i) {
-    *next_float++ = y;
-    *next_float++ = x;
-    *next_float++ = TILE_BLIND + i;
-    *next_float++ = 3;
-    *next_float_time++ = FLOAT_FRAMES;
+    float_add_ex(x, y, TILE_BLIND + i, 3);
     x += 8;
   }
 }
@@ -64,11 +73,7 @@ void float_pickup(PickupType ptype) {
   u8 y = POS_TO_Y(pos);
 
   for (i = flt_start; i < flt_end; ++i) {
-    *next_float++ = y;
-    *next_float++ = x;
-    *next_float++ = float_pick_type_tiles[i];
-    *next_float++ = 3;
-    *next_float_time++ = FLOAT_FRAMES;
+    float_add_ex(x, y, float_pick_type_tiles[i], 3);
     x += 8;
   }
 }
@@ -77,16 +82,21 @@ void float_update(void) {
   // Draw float sprites and remove them if timed out
   u8 *flt = (u8 *)float_sprites;
   u8 *flt_time = float_times;
-  while (flt != next_float) {
+  while (flt != last_float) {
     if (--*flt_time == 0) { // float time
-      next_float -= 4;
-      --next_float_time;
-      if (flt != next_float) {
-        flt[0] = next_float[0];
-        flt[1] = next_float[1];
-        flt[2] = next_float[2];
-        flt[3] = next_float[3];
-        *flt_time = *next_float_time;
+      if (next_float == last_float) {
+        next_float -= 4;
+        --next_float_time;
+      }
+      // Remove this float by swapping with the last in the list.
+      last_float -= 4;
+      --last_float_time;
+      if (flt != last_float) {
+        flt[0] = last_float[0];
+        flt[1] = last_float[1];
+        flt[2] = last_float[2];
+        flt[3] = last_float[3];
+        *flt_time = *last_float_time;
       }
       continue;
     } else if (flt[0] > 16) {
