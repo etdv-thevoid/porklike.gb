d5d22c9e68d52dc4239f5a924aca4e8760898399
etdv-thevoid <9028176+etdv-thevoid@users.noreply.github.com>
2025-09-19 15:28:34 -0500

    small changes

--- titchack.sh
+++ titchack.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./ref/titchack.py ./build/gb/porklike.gb '$13C' --checksum '0x8C' --fixlicensee yes --headeraddress '#0x14D' --type both
+./ref/titchack.py ./build/gb/porklike.gb '$13C' --checksum '#0x8B' --fixlicensee yes --headeraddress '#0x14D' --type both
